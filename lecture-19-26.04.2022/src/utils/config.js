c0f4079453631d1b69633cfac2ec0536a818f1c6
Дмитрий Василенко <dmitrijvasilenko@MacBook-Air-Dmitrij.local>
2022-05-01 11:49:31 +0300

    lecture 19, 26.04.2022

---
+++ lecture-19-26.04.2022/src/utils/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    url: 'https://api.react-learning.ru',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjRmMjVlZGFlMTlmNTQ2ZGMwODNhNGYiLCJpYXQiOjE2NDkzNTQzNDgsImV4cCI6MTY4MDg5MDM0OH0.fy000tsf328VEUC-z-mevFwEfXR_5FDI7SnDURInET0',
+};
+
+
+// еще один
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU4NzA0MzBjZGQ3ZDNmZDUyZjgyZWUiLCJpYXQiOjE2NDk5NjMxNTUsImV4cCI6MTY4MTQ5OTE1NX0.JatucySGsawbVKuLbSS-wfqoh8HbpwPbz9JLUy71dvw
